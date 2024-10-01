26e6c0c5541f42fe02c38126f7bfd4b9cc1e784a
karan16kkg <karankumargarg1610@gmail.com>
2024-10-01 10:03:17 +0530

    test

--- Frontend/src/components/temp/Tempapp.jsx
+++ Frontend/src/components/temp/Tempapp.jsx
@@ -13,28 +13,15 @@ const Tempapp = () => {
   }
   let z = data.country;
 
-
-  // useEffect(() => {
-  //   axios.get(`https://api.openweathermap.org/data/2.5/weather?q=chandigarh&appid=a322d39c25aa93e12bfe3ff1a6c68891`)
-  //   .then((response)=>{
-  //     let x=response.data;
-  //     console.log(x);
-  //     setcity(x.name);
-  //     let y = Math.round(x.main.temp - 273);
-  //     settemp(y)
-  //   })
-  // },[])
-  
   console.log(city);
   console.log(temp);
-  const getdata = ()=>{
+  const getdata = async()=>{
     setcity(z);
     console.log(city);
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a322d39c25aa93e12bfe3ff1a6c68891`)
+    await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a322d39c25aa93e12bfe3ff1a6c68891`)
     .then((response)=>{
       let x=response.data;
       console.log(x);
-      // setcity(x.name);
       let y = Math.round(x.main.temp - 273);
       settemp(y)
     })
@@ -45,7 +32,7 @@ const Tempapp = () => {
       <div id='home' className='h-screen'>
         <div className='mt-14 h-14 items-center stati justify-center flex h-fit'>
           <input value={data.country} className='w-1/3 h-12 text-xl rounded-full px-5 border-2 border-purple-500 outline-none ml-28 mt-8' type="search" name="country" onChange={handleChange} />
-          <button className='text-white text-3xl border border-purple-700 rounded-l-full rounded-r-full px-4 h-12 bg-purple-500 mt-8' onClick={()=>{getdata()}}>Search</button>
+          <button className='text-white text-3xl border border-purple-700 rounded-l-full rounded-r-full px-4 h-12 bg-purple-500 mt-8' onClick={getdata}>Search</button>
         </div>
         <div className='text-white absolote flex flex-col items-center mt-10'>
           <h1 className='top-80 text-9xl'>{temp}°C</h1>
